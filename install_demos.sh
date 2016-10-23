addea39f7ec16419f48e2cd719554878b3da2e58
Marco Bernasocchi <marco@opengis.ch>
2016-10-23 12:35:36 +0700

    initial work for updating auth db

--- install_demos.sh
+++ install_demos.sh
@@ -9,3 +9,23 @@ docker exec $DB_CONTAINER chmod +x /tmp/load_sql.sh
 docker exec $DB_CONTAINER /tmp/load_sql.sh
 
 # todo add repository
+LIZMAP_CONFIG=lizmap_var/config/lizmapConfig.ini.php
+echo "[repository:demo]" >> $LIZMAP_CONFIG
+echo "label=demo" >> $LIZMAP_CONFIG
+echo "path="/home/"" >> $LIZMAP_CONFIG
+echo "allowUserDefinedThemes=1" >> $LIZMAP_CONFIG
+
+
+#docker exec -it $LIZMAP_CONTAINER bash -c "apt-get -y install sqlite3"
+#BEGIN TRANSACTION;
+#INSERT INTO `jacl2_rights` VALUES ('lizmap.repositories.view','__anonymous','demo',0);
+#INSERT INTO `jacl2_rights` VALUES ('lizmap.repositories.view','admins','demo',0);
+#INSERT INTO `jacl2_rights` VALUES ('lizmap.tools.displayGetCapabilitiesLinks','__anonymous','demo',0);
+#INSERT INTO `jacl2_rights` VALUES ('lizmap.tools.displayGetCapabilitiesLinks','admins','demo',0);
+#INSERT INTO `jacl2_rights` VALUES ('lizmap.tools.edition.use','__anonymous','demo',0);
+#INSERT INTO `jacl2_rights` VALUES ('lizmap.tools.edition.use','admins','demo',0);
+#INSERT INTO `jacl2_rights` VALUES ('lizmap.tools.layer.export','__anonymous','demo',0);
+#INSERT INTO `jacl2_rights` VALUES ('lizmap.tools.layer.export','admins','demo',0);
+#INSERT INTO `jacl2_rights` VALUES ('lizmap.tools.loginFilteredLayers.override','__anonymous','demo',0);
+#INSERT INTO `jacl2_rights` VALUES ('lizmap.tools.loginFilteredLayers.override','admins','demo',0);
+#COMMIT;
